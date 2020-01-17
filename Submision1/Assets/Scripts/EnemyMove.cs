b49106fc106ffd26a728b66552273467bc7f4c75
Bulu507 <53290382+Bulu507@users.noreply.github.com>
2020-01-18 04:13:25 +0700

    commit master submission1

---
+++ Submision1/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    #region Enums
+
+    #endregion //Enums
+
+    #region Public Variables
+
+    public Sprite[] sprites;
+
+    #endregion //Public Variables
+
+    #region Private Variables
+
+    [SerializeField]
+    private float moveDirection = 1;
+    private float randomSpeed;
+    private float randomScale;
+    private float randomVelocityY;
+    private Rigidbody2D rb;
+
+    #endregion //Private Variables
+
+    // (Unity Named Methods)
+    #region Main Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpeed = Random.Range(30, 70); 
+        randomScale = Random.Range(30, 80);
+        randomVelocityY = Random.Range(200, 220);
+        int index = Random.Range(0, sprites.Length);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[index];
+        transform.localScale = new Vector3(randomScale, randomScale, 0);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = (randomSpeed * Time.deltaTime * moveDirection) + transform.position.x;
+        transform.position = new Vector3(move, transform.position.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // make a stable bounce
+        rb.velocity = new Vector2(rb.velocity.x, randomVelocityY);
+    }
+
+    #endregion //Main Methods
+
+    //(Custom Named Methods)
+    #region Utility Methods 
+
+    #endregion //Utility Methods
+
+    //Coroutines run parallel to other fucntions
+    #region Coroutines
+
+    #endregion //Coroutines
+}
+
---
+++ Submision1/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    #region Enums
+
+    #endregion //Enums
+
+    #region Public Variables
+
+    #endregion //Public Variables
+
+    #region Private Variables
+
+    [SerializeField]
+    private float xBoundaries = 576;
+    private Animator anim;
+    private Rigidbody2D rb;
+    private bool facingRight;
+    private Vector3 localScale;
+    private GameObject player;
+    private string IsMove = "IsMove";
+
+    #endregion //Private Variables
+
+    // (Unity Named Methods)
+    #region Main Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * 500f * Time.deltaTime, 0f, 0f);
+        anim.SetBool(IsMove, false);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xBoundaries, xBoundaries),
+            transform.position.y, transform.position.z); //boundaries
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            anim.SetBool(IsMove, true);
+            facingRight = false;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetBool(IsMove, true);
+            facingRight = true;
+        }
+
+        flip(facingRight);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.CompareTag("Enemy"))
+        //{
+        //    //Destroy(collision.gameObject);
+        //    SceneManager.LoadScene("GameOver"); // Baris Ini akan digunakan pada submodul GameOver
+        //}
+    }
+
+    #endregion //Main Methods
+
+    //(Custom Named Methods)
+    #region Utility Methods 
+
+    private void flip(bool fc)
+    {
+        if ((fc && (localScale.x < 0)) || (!fc && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
+
+        transform.localScale = localScale;
+    }
+
+    #endregion //Utility Methods
+
+    //Coroutines run parallel to other fucntions
+    #region Coroutines
+
+    #endregion //Coroutines
+}
